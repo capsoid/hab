c5ef701fe9ded88f7755affe2b4e8c37f8770df4
Vasili Slapik <vasili_slapik@epam.com>
2018-12-03 16:35:09 +0300

    Enable zephyr shell

--- cc/src/main.c
+++ cc/src/main.c
@@ -26,10 +26,11 @@ void main(void)
 		sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
 		sensor_channel_get(dev, SENSOR_CHAN_PRESS, &press);
 		sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &humidity);
-
+/*
 		printf("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
 		      temp.val1, temp.val2, press.val1, press.val2,
 		      humidity.val1, humidity.val2);
+        */
 
 		k_sleep(1000);
 	}
