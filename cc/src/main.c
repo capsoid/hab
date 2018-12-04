5d71a15ae83f3526682dfecd00a8b2d21a06ef8d
Vasili Slapik <vslapik@gmail.com>
2018-12-04 15:51:57 +0300

    A bit more of SPI

--- cc/src/main.c
+++ cc/src/main.c
@@ -12,26 +12,41 @@
 #include <spi.h>
 #include <stdio.h>
 
+struct spi_config spi_config = {
+    .frequency = 24000000,
+    .operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(8),
+    .slave = 0,
+    .cs = NULL,
+};
+
+u8_t cmd = 42;
+struct spi_buf tx_buf = { .buf = &cmd, .len = 1 };
+struct spi_buf_set tx_bufs = { .buffers = &tx_buf, .count = 1 };
+
 void main(void)
 {
-	printk("[%s  %s] Hello World from %s!\n", __DATE__, __TIME__, CONFIG_BOARD);
-	struct device *dev = device_get_binding("BME280");
+    printk("[%s  %s] Hello World from %s!\n", __DATE__, __TIME__, CONFIG_BOARD);
 
-	printf("dev %p name %s\n", dev, dev->config->name);
+    struct device *bme = device_get_binding("BME280");
+    struct device *cc1101 = device_get_binding("SPI_1");
 
-	while (1) {
-		struct sensor_value temp, press, humidity;
+    printf("dev %p name %s\n", bme, bme->config->name);
 
-		sensor_sample_fetch(dev);
-		sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
-		sensor_channel_get(dev, SENSOR_CHAN_PRESS, &press);
-		sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &humidity);
-/*
-		printf("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
-		      temp.val1, temp.val2, press.val1, press.val2,
-		      humidity.val1, humidity.val2);
-        */
+    spi_write(cc1101, &spi_config, &tx_bufs);
 
-		k_sleep(1000);
-	}
+    while (1) {
+        struct sensor_value temp, press, humidity;
+
+        sensor_sample_fetch(bme);
+        sensor_channel_get(bme, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+        sensor_channel_get(bme, SENSOR_CHAN_PRESS, &press);
+        sensor_channel_get(bme, SENSOR_CHAN_HUMIDITY, &humidity);
+        /*
+           printf("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
+           temp.val1, temp.val2, press.val1, press.val2,
+           humidity.val1, humidity.val2);
+           */
+
+        k_sleep(1000);
+    }
 }
